ce549f8f53a91857be9455b305590acc7a0900e2
astroturk <astroturk26@gmail.com>
2021-08-08 16:59:14 +0530

    first commit

---
+++ assignment0/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+	int x = 1;
+
+	printf("Hello x = %d\n", x);
+
+	asm("incl %0" :"+r"(x));
+
+	printf("Hello x = %d after increment\n", x);
+
+	if (x == 2){
+		printf("OK\n");
+	} else {
+		printf("ERROR\n");
+	}
+}
